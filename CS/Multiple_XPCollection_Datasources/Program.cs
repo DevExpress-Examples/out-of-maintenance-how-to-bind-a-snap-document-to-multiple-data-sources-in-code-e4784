96a204bc1a37b844487e3e95f8f7b1864e34937a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Multiple_XPCollection_Datasources/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+
+namespace Multiple_XPCollection_Datasources {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            XpoDefault.DataLayer = new SimpleDataLayer(new InMemoryDataStore());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ VB/Multiple_XPCollection_Datasources/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Forms
+Imports DevExpress.Xpo
+' ...
+
+Namespace Multiple_XPCollection_Datasources
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+			Dim session As New Session()
+
+			Dim products = New XPCollection(Of Product)(session)
+			Dim TempProduct As Product = New Product(session)
+            TempProduct.ProductName = "Chai"
+            TempProduct.Save()
+            TempProduct = New Product(session)
+            TempProduct.ProductName = "Ipoh Coffee"
+            TempProduct.Save()
+            TempProduct = New Product(session)
+            TempProduct.ProductName = "Aniseed Syrup"
+            TempProduct.Save()
+
+			Dim categories = New XPCollection(Of Category)(session)
+			Dim TempCategory As Category = New Category(session)
+            TempCategory.CategoryName = "Beverages"
+            TempCategory.Save()
+            TempCategory = New Category(session)
+            TempCategory.CategoryName = "Condiments"
+            TempCategory.Save()
+            TempCategory = New Category(session)
+            TempCategory.CategoryName = "Seafood"
+            TempCategory.Save()
+
+			Me.snapControl1.Document.BeginUpdateDataSource()
+			Me.snapControl1.Document.DataSources.Add("Products", products)
+			Me.snapControl1.Document.DataSources.Add("Categories", categories)
+			Me.snapControl1.Document.EndUpdateDataSource()
+		End Sub
+	End Class
+End Namespace
